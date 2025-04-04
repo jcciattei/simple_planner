cec3afa31c4e695bd8d151377c1fb6dd6b87749e
jcciattei <jcia1@umbc.edu>
2025-04-04 19:53:32 -0400

    Display courses when a major is selected

--- backend/sample_data.py
+++ backend/sample_data.py
@@ -1,17 +1,63 @@
 TITLE = "Simple Planner"
+# Define the courses collection
+# This is a list of dictionaries, where each course is a dictionary
+# with an id, number, description
+# Note that id's are hardcoded for now for ease of development
+
+courses = [
+    {
+        "_id": 101,
+        "number": "MATH151",
+        "description": "Calculus and Analytic Geometry I"
+    },
+    {
+        "_id": 102,
+        "number": "MATH152",
+        "description": "Calculus and Analytic Geometry II"
+    },
+        {
+        "_id": 103,
+        "number": "CMSC201",
+        "description": "Computer Science I"
+    },
+        {
+        "_id": 104,
+        "number": "CMSC202",
+        "description": "Computer Science II"
+    },
+        {
+        "_id": 105,
+        "number": "PHYS121",
+        "description": "Introductory Physics I"
+    },
+    {
+        "_id": 106,
+        "number": "ENES101",
+        "description": "Introduction to Engineering"
+    },
+        {
+        "_id": 107,
+        "number": "ENME110",
+        "description": "Statics"
+    },
+]
 
 # Define the majors collection
 # This is a list of dictionaries, where each major is a dictionary
 # with an id, name, credit count
+# Note that id's are hardcoded for now for ease of development
+
 majors = [
     {
         "_id": 1,
         "name": "Computer Science Gateway",
-        "number_credits": 12
+        "number_credits": 12,
+        "required_courses": [101, 103, 104]
     },
     {
         "_id": 2,
         "name": "Mechanical Engineering Gateway",
-        "number_credits": 19
+        "number_credits": 19,
+        "required_courses": [101, 102, 105, 106, 107]
     }
 ]
--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@ from flask import Flask, send_from_directory
 from flask_restful import Api
 from resources.title import Title
 from resources.majors import Majors
+from resources.major_courses import MajorCourses
 
 app = Flask(__name__, static_folder="../frontend/simple-planner-app/dist", static_url_path="/")
 api = Api(app)
@@ -10,6 +11,8 @@ api = Api(app)
 api.add_resource(Title, '/api/v1/title')
 # Resource for the list of majors
 api.add_resource(Majors, '/api/v1/majors')
+# Resource for the list of courses within a major
+api.add_resource(MajorCourses, '/api/v1/majors/<int:major_id>/courses')
 
 # Flask will serve React
 @app.route('/')
